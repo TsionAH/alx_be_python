66f380017daa9b1d169e4917861ec70363ddca9b
TsionAH <tsionalemuhandiso@gmail.com>
2025-07-16 13:35:43 -0700

    fixed

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,7 @@
 nums_row = int(input("Enter the size of the pattern: "))
-for i in range (nums_row):
-    for j in range (nums_row):
-        print("*" , end="")
+i = 0
+while i < nums_row:
+    for j in range(nums_row):
+        print("*", end="")
     print()
-    
+    i += 1   
